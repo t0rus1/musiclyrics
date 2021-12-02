c2e00a5190f55c099e7933d17f349d8a2b9e9c26
t0rus1 <leonvandyk@gmail.com>
2021-12-02 17:06:30 +0200

    UI improvement

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import os.path
+from youtube_dl.utils import DownloadError
 import streamlit as st
 import re, requests, subprocess, urllib.parse, urllib.request
 from bs4 import BeautifulSoup
@@ -15,7 +17,7 @@ def init_folders():
     try:
         os.mkdir('./temp')
     except OSError as error:
-        print(error)
+        pass #print(error)
 
 def setup_page():
     ''' streamlit page config, app title etc'''
@@ -49,13 +51,12 @@ def submit_youtube_query(music_name):
     return re.findall(r"watch\?v=(\S{11})", formatUrl.read().decode())
 
 @st.cache
-def get_youtube_titles(search_results, page_size): #, prog_bar):
+def get_youtube_links(search_results, page_size): #, prog_bar):
     '''
     return dict of yt urls keyed on title
     '''
-
     yt_urls={}
-    with st.spinner("getting youtube titles..."):
+    with st.spinner("get youtube links..."):
         for index in range(0, page_size):
             clip_url = "https://www.youtube.com/watch?v=" + "{}".format(search_results[index])
             clip = requests.get(clip_url)
@@ -81,15 +82,50 @@ def get_youtube_titles(search_results, page_size): #, prog_bar):
 
 def search_form_callback():
     ''' executed when form is submitted and before re-run '''
-    chosen_title = st.session_state.title_radio
+
+    if st.session_state.music_query == '':
+        return
+
+    if not hasattr(st.session_state,'title_radio'):
+        return
+
+    chosen_title = st.session_state.title_radio 
+    # dont download title already in the player
+    most_recently_download = hasattr(st.session_state,'current_playee') and chosen_title == st.session_state.current_playee
+
     download_url = st.session_state.yt_urls[chosen_title]
-    with st.spinner(f'downloading audio for {chosen_title}'):
-        completion_state = downloader.download(download_url)
-        if completion_state is not None:
-            st.exception(completion_state)
-        else:
-            st.success(f'audio for {chosen_title} successfully downloaded!')
-    st.audio(f'./temp/{chosen_title}.mp3')
+    if not most_recently_download:
+        with st.spinner(f"Downloading and extracting audio from '{chosen_title}' youtube video ..."):
+            placeholder = st.empty()
+            completion_state = downloader.download(download_url, placeholder)
+            if completion_state is not None:
+                if isinstance(completion_state, DownloadError):
+                    st.error(f'This video ({chosen_title}) may only being played on Youtube itself')
+                    print(completion_state)
+                elif isinstance(completion_state,FileNotFoundError):
+                    st.error('File not found - title may contain disallowed characters...')
+                else:
+                    st.exception(completion_state)
+            else:
+                st.markdown(f'** {chosen_title} **')
+
+    # youtube_dl alters some filename chars, so
+    # ensure certain disallowed characters in filenames get 
+    # substituted in the same way that youtube_dl does them
+    scrubbed_title = re.sub(r'[\|/\\]', '_', chosen_title)
+    # double quotes become single
+    scrubbed_title = re.sub(r'"', "'", scrubbed_title)
+    # : becomes -
+    scrubbed_title = re.sub(r':', "-", scrubbed_title)
+
+    playee_fn = f'./temp/{scrubbed_title}.mp3'
+    if os.path.isfile(playee_fn):
+        st.session_state.current_playee = chosen_title
+        # place audio player - hopefully the file will be an mp3:
+        st.audio(playee_fn, format='audio/ogg')
+        st.write(f'Watch video on Youtube {download_url}')
+    else:
+        st.error(f'Unable to find or play {playee_fn} - the audio player cannot load it due to irregular titling.')
             
 
 ###################################
@@ -100,25 +136,30 @@ setup_page()
 #get help on any widget this way
 #st.help(st.selectbox)
 
+with st.expander("options"):
+    results_size = int(st.number_input('number of results to fetch',min_value=1, max_value=100, value=DEFAULT_PAGE_SIZE))
+    video_links_only = st.checkbox('get youtube videos links only',value=False, help='check this if you only want to watch videos, as opposed to the normal pairing of audio with lyrics')
+
 with st.form('search_form'):
-    music_query = st.text_input("Artist and name of song video to search for")
+    #results_size = DEFAULT_PAGE_SIZE        
+    music_query = st.text_input("Artist and|or name of song video to search for", key='music_query', help='Artist AND song OR just artist OR just song. To begin a new search, clear this text box and click submit below')    
     if not music_query:
         submit_button = st.form_submit_button('search')
         st.warning('please enter a search term eg Αντώνης Ρέμος')
         st.stop()
     else:
-        #results_page_size = st.number_input('Number of results:',min_value=1, max_value=100, value=DEFAULT_PAGE_SIZE, key='pagesize_input')
-        results_page_size = DEFAULT_PAGE_SIZE
         search_results = submit_youtube_query(music_query)
         # inspect search results and extract video urls
-        st.session_state.yt_urls=get_youtube_titles(search_results, results_page_size)
+        st.session_state.yt_urls=get_youtube_links(search_results, results_size)
 
-        # with st.container():
-        #     for option in yt_urls.keys():
-        #         st.write(f'{option}')
-        #         downloader.download(yt_urls[option])
-
-        st.radio('Choose a title to download and play', st.session_state.yt_urls.keys(), key='title_radio')
+        if video_links_only:
+            # show only youtube links in a list
+            for title,link in st.session_state.yt_urls.items():
+                st.markdown(f'[{title}]({link})')
+        else:
+            # intended default mode
+            st.radio('Choose a title to download and play', st.session_state.yt_urls.keys(), key='title_radio')
+        
         submit_button = st.form_submit_button(label='submit', on_click=search_form_callback)
 
 
--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import streamlit as st
 import youtube_dl
 
 
@@ -14,12 +15,21 @@ class MyLogger(object):
 
 
 def my_hook(d):   
-    if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
-    # else:
-    #     print(d['status'])
+    if d['status'] == 'finished': # could alo be 'downloading' or 'error'
+        progress_message = '' #'Done downloading, now converting ...')
+    elif d['status'] == 'downloading':
+        # format a progress message
+        # bytes_so_far = d['downloaded_bytes']
+        # bytes_total = d['total_bytes']
+        eta = d['eta']
+        bytes_so_far = "{:,}".format(d['downloaded_bytes']) 
+        bytes_total = "{:,}".format(d['total_bytes'])
+        progress_message = f"{bytes_so_far} of {bytes_total} bytes. ETA: {eta} seconds ..."
+    else:
+        progress_message = d['status']
 
-
+    with ydl_opts['placeholder']:
+        st.write(progress_message)
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -30,16 +40,29 @@ ydl_opts = {
     }],
     'logger': MyLogger(),
     'progress_hooks': [my_hook],
-    'outtmpl': './temp/%(title)s.%(ext)s'
+    'outtmpl': './temp/%(title)s.%(ext)s',
+    'placeholder': None,
+    'continuedl': True,
+    #'max_filesize': 10*1024*1024
 }
 
-def download(url):
+def download(url, placeholder):
+    ''' use youtube_dl to download video 
+        http://www.youtube.com/watch?v=nrGk0AuFd_9
+        or
+        https://youtu.be/VamaZaxdO70  
 
+        regex like so 
+        /^[A-Za-z0-9_-]{11}$/
+    '''
+
+    ydl_opts['placeholder'] = placeholder
     dl_error = None
     try:
         #youtube_dl.YoutubeDL( params={'-c': '', '--no-mtime': '', 'outtmpl': './%(uploader)s/%(title)s-%(upload_date)s-%(id)s.%(ext)s'} ).download([url])        
         #youtube_dl.YoutubeDL( params={'-c': '', '--no-mtime': '', 'outtmpl': './%(title)s-%(id)s.%(ext)s'} ).download([url])        
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            #print(ydl.params)
             ydl.download([url])
     except youtube_dl.DownloadError as de:
          dl_error = de
